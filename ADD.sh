0aa6e00f6f4bb413cd6608b5cbe347d344448b02
saitarun135 <saitarun135@gm.com>
2021-02-20 11:26:29 +0530

    MUL: adding multiplication feature

--- ADD.sh
+++ ADD.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 read -p "enter 1st num:" num1;
 read -p "enter 2nd num:" num2;
-result=$(($num1+$num2))
-echo  $result
+resultAdd=$(($num1+$num2))
+resultMul=$(($num1*num2))
+echo  $resultAdd
+echo  $resultMul
